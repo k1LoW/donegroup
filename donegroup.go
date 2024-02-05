4a0977daebd13bfa07d9afd89dff82536f0a6d85
k1LoW <k1lowxb@gmail.com>
2024-02-05 23:01:21 +0900

    bonsai

--- donegroup.go
+++ donegroup.go
@@ -10,14 +10,15 @@ import (
 
 var doneGroupKey = struct{}{}
 
+// doneGroup is cleanup function groups per Context
 type doneGroup struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-
-	// context.Context for after the context is canceled
+	// ctxw is the context used to call the cleanup functions
 	ctxw          context.Context
 	cleanupGroups []*errgroup.Group
 	mu            sync.Mutex
+	// _ctx, _cancel is a context/cancelFunc used to set dg.ctxw
+	_ctx    context.Context
+	_cancel context.CancelFunc
 }
 
 // WithCancel returns a copy of parent with a new Done channel and a doneGroup.
@@ -32,26 +33,26 @@ func WithCancelWithKey(ctx context.Context, key any) (context.Context, context.C
 	if !ok {
 		ctx, cancel := context.WithCancel(context.Background())
 		dg = &doneGroup{
-			ctx:    ctx,
-			cancel: cancel,
+			_ctx:    ctx,
+			_cancel: cancel,
 		}
 	}
 	eg := new(errgroup.Group)
 	dg.cleanupGroups = append(dg.cleanupGroups, eg)
 	secondDg := &doneGroup{
-		ctx:           dg.ctx,
-		cancel:        dg.cancel,
+		_ctx:          dg._ctx,
+		_cancel:       dg._cancel,
 		cleanupGroups: []*errgroup.Group{eg},
 	}
 	return context.WithValue(secondCtx, key, secondDg), secondCancel
 }
 
-// Clenup runs f when the context is canceled.
+// Clenup registers a function to be called when the context is canceled.
 func Clenup(ctx context.Context, f func(ctx context.Context) error) error {
 	return ClenupWithKey(ctx, doneGroupKey, f)
 }
 
-// ClenupWithKey runs f when the context is canceled.
+// ClenupWithKey Clenup registers a function to be called when the context is canceled.
 func ClenupWithKey(ctx context.Context, key any, f func(ctx context.Context) error) error {
 	dg, ok := ctx.Value(key).(*doneGroup)
 	if !ok {
@@ -61,7 +62,7 @@ func ClenupWithKey(ctx context.Context, key any, f func(ctx context.Context) err
 	first := dg.cleanupGroups[0]
 	first.Go(func() error {
 		<-ctx.Done()
-		<-dg.ctx.Done()
+		<-dg._ctx.Done()
 		dg.mu.Lock()
 		ctx := dg.ctxw
 		dg.mu.Unlock()
@@ -70,22 +71,22 @@ func ClenupWithKey(ctx context.Context, key any, f func(ctx context.Context) err
 	return nil
 }
 
-// Wait blocks until the context is canceled.
+// Wait blocks until the context is canceled. Then calls the function registered by Cleanup.
 func Wait(ctx context.Context) error {
 	return WaitWithKey(ctx, doneGroupKey)
 }
 
-// Wait blocks until the context (ctx) is canceled. Then calls the function registered in Cleanup with context (ctxw).
+// Wait blocks until the context (ctx) is canceled. Then calls the function registered by Cleanup with context (ctxw).
 func WaitWithContext(ctx, ctxw context.Context) error {
 	return WaitWithContextAndKey(ctx, ctxw, doneGroupKey)
 }
 
-// WaitWithKey blocks until the context is canceled.
+// WaitWithKey blocks until the context is canceled. Then calls the function registered by Cleanup.
 func WaitWithKey(ctx context.Context, key any) error {
 	return WaitWithContextAndKey(ctx, context.Background(), key)
 }
 
-// WaitWithKeyAndContext blocks until the context is canceled. Then calls the function registered in Cleanup with context (ctxx).
+// WaitWithKeyAndContext blocks until the context is canceled. Then calls the function registered by Cleanup with context (ctxx).
 func WaitWithContextAndKey(ctx, ctxw context.Context, key any) error {
 	dg, ok := ctx.Value(key).(*doneGroup)
 	if !ok {
@@ -95,7 +96,7 @@ func WaitWithContextAndKey(ctx, ctxw context.Context, key any) error {
 	dg.ctxw = ctxw
 	dg.mu.Unlock()
 	<-ctx.Done()
-	dg.cancel()
+	dg._cancel()
 	eg, _ := errgroup.WithContext(ctxw)
 	for _, g := range dg.cleanupGroups {
 		eg.Go(g.Wait)
